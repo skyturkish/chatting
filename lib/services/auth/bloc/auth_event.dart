d24c6f38f11d4b2df5e9b683011f59df8323b238
skyturkish <gokturk.acr2002@gmail.com>
2022-07-30 20:39:39 +0300

    beforebigmove

--- lib/services/auth/bloc/auth_event.dart
+++ lib/services/auth/bloc/auth_event.dart
@@ -9,11 +9,17 @@ class AuthEventitialize extends AuthEvent {
   const AuthEventitialize();
 }
 
+class AuthEventInitialize extends AuthEvent {
+  const AuthEventInitialize();
+}
+
 class AuthEventLogIn extends AuthEvent {
   final String email;
   final String password;
   const AuthEventLogIn(this.email, this.password);
 }
+
+class AuthEventRegister extends AuthEvent {}
 
 class AuthEventLogOut extends AuthEvent {
   const AuthEventLogOut();
